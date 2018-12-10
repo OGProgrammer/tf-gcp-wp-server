0204c628bd0d5930f33b9603d74c0f63547f0bf0
Joshua Copeland <joshuarcopeland@gmail.com>
2018-12-10 07:26:08 -0800

    Added internal ip output

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,10 @@ output "server_ip" {
   value = "${google_compute_address.main.address}"
 }
 
+output "server_internal_ip" {
+  value = "${google_compute_instance.main.network_interface.0.access_config.0.nat_ip}"
+}
+
 output "server_location" {
   value = "${google_compute_instance.main.zone}"
 }
