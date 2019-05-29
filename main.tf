5e067c3039481b0a9de0045ce9a01d3be52965c1
Joshua Copeland <Josh@RemoteDevForce.com>
2019-05-29 09:58:56 -0700

    Added terraform 0.12 support

--- main.tf
+++ main.tf
@@ -5,31 +5,31 @@ resource "google_compute_address" "main" {
 
 # Use this to find the latest
 data "google_compute_image" "debian" {
-  family = "debian-9"
+  family  = "debian-9"
   project = "debian-cloud"
 }
 
 # Single Instance + Debian 9 Bash Provision script
 resource "google_compute_instance" "main" {
-  name = "tf-${var.vendor}-wp-${var.server-tag}"
-  machine_type = "${var.machine-type}"
-  zone = "${var.region}-${var.zone}"
-  deletion_protection = "${var.protect-from-deletion}"
+  name                = "tf-${var.vendor}-wp-${var.server-tag}"
+  machine_type        = var.machine-type
+  zone                = "${var.region}-${var.zone}"
+  deletion_protection = var.protect-from-deletion
 
   boot_disk {
     auto_delete = "false"
     initialize_params {
-      image = "${data.google_compute_image.debian.self_link}"
-      size = "30"
+      image = data.google_compute_image.debian.self_link
+      size  = "30"
     }
   }
 
   network_interface {
-    subnetwork = "${var.subnetwork}"
+    subnetwork = var.subnetwork
 
     # Attach global static IP address to instance
-    access_config = {
-      nat_ip = "${google_compute_address.main.address}"
+    access_config {
+      nat_ip = google_compute_address.main.address
     }
   }
 
@@ -38,32 +38,34 @@ resource "google_compute_instance" "main" {
     "https-server",
     "http-server",
     "ssh",
-    "pingable"
+    "pingable",
   ]
 
-  metadata {
-    env = "${var.env}"
-    company = "${var.vendor}"
-    label = "wp-${var.server-tag}"
-    server = "tf-${var.vendor}-wp-${var.server-tag}"
+  metadata = {
+    env       = var.env
+    company   = var.vendor
+    label     = "wp-${var.server-tag}"
+    server    = "tf-${var.vendor}-wp-${var.server-tag}"
     managedBy = "terraform"
   }
 
-  labels {
-    env = "${var.env}"
-    company = "${var.vendor}"
-    label = "wp-${var.server-tag}"
-    server = "tf-${var.vendor}-wp-${var.server-tag}"
+  labels = {
+    env        = var.env
+    company    = var.vendor
+    label      = "wp-${var.server-tag}"
+    server     = "tf-${var.vendor}-wp-${var.server-tag}"
     managed_by = "terraform"
   }
 
   ## Pipe the server setup bash script into the GCP server init script
-  metadata_startup_script = "${file("wp-server-deb9.sh")}"
+  metadata_startup_script = file("wp-server-deb9.sh")
+
   # If changes are made to the server bash provision script, lets not recreate the server over that.
   lifecycle {
     ignore_changes = [
-      "metadata_startup_script",
-      "boot_disk.initialize_params"
+      metadata_startup_script,
+      boot_disk[0].initialize_params,
     ]
   }
 }
+
--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,16 @@
 output "server_name" {
-  value = "${google_compute_instance.main.name}"
+  value = google_compute_instance.main.name
 }
 
 output "server_ip" {
-  value = "${google_compute_address.main.address}"
+  value = google_compute_address.main.address
 }
 
 output "server_internal_ip" {
-  value = "${google_compute_instance.main.network_interface.0.address}"
+  value = google_compute_instance.main.network_interface[0].address
 }
 
 output "server_location" {
-  value = "${google_compute_instance.main.zone}"
+  value = google_compute_instance.main.zone
 }
+
